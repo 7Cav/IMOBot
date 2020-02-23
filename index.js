ceec462c8845928225f3839377f36c37b8b24f4c
VexA3 <vexhd0@gmail.com>
2020-02-23 02:08:09 -0600

    updated index.js

--- index.js
+++ index.js
@@ -7,10 +7,12 @@
 // Dependencies
 const Discord = require('discord.js');
 const config = require('./config.json');
-const fs = require("fs")
+const fs = require("fs");
+let data = require('./data.json');
 const bot = new Discord.Client();
 const botLogin = config.Login;
 
+// ******* START OF API STUFF *******
 // Axios and Cav API
 const AUTH_TOKEN = config.CavAPIToken;
 const axios = require('axios').default;
@@ -24,19 +26,48 @@ const instance = axios.create({
     }
 })
 
+// Concept API call to get updated information from API.
 // Return information from the API and put it into ./data.json
-// GET: /users/active
-instance({
-    method: "GET",
-    url: "https://api.7cav.us/v1/users/active",
-    responseType: "stream"
-}).then(function (response) {
-    response.data.pipe(fs.createWriteStream(JSON.stringify()));
-})
+function getAPI() {
+    // GET: /users/active
+    instance({
+        method: "GET",
+        url: "https://api.7cav.us/v1/users/active",
+        responseType: "stream"
+    }).then(function (response) {
+        response.data.pipe(fs.createWriteStream(JSON.stringify()));
+ })
+}
+// ******* END OF API STUFF *******
 
 //When the bot is ready.
 bot.on('ready', () => {
     console.log("Connected as " + bot.user.tag);
+
+    // This is inside 'ready' because discord api can only be interacted with inside an event handler...
+    // Role Assignment Concept:
+    let userCache = [
+        {
+            discord_id: "201893080805146624",
+            username: "Vex"
+        },
+        {
+            discord_id: "104461066662060032",
+            username: "Jarvis"
+        }
+    ];
+    
+    function doSync() {
+        let users = userCache; // in future this would be a db call
+        let server = bot.guilds.get('discordServerID');
+     
+        users.forEach(user => {
+            let discordProfile = server.members.get(user.discord_id);
+            discordProfile.addRole('roleID'); // genstaff role
+        })
+    }
+
+    doSync();
 });
 
 // Crash reporting
