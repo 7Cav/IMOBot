40b666c0847a5b2d83432433320f16fd09379ef9
VexA3 <vexhd0@gmail.com>
2020-03-16 23:03:43 -0500

    changed variable names and fixed formatting

--- Methods/doSync.js
+++ Methods/doSync.js
@@ -3,49 +3,46 @@ const bot = new Discord.Client();
 const config = require('../config/main.json');
 
 module.exports.run = async (API, members) => {
-    var mems = API;
+    var users = API;
     let discordProfile = members
     console.log(discordProfile);
     
     // Assign Billet Roles:
     mems.forEach(user => {
-        // ERROR: discordProfile.forEach is not a function
+            // ERROR: discordProfile.forEach is not a function
         discordProfile.forEach(mem => {
-            // Sync Ranks
-        let shortRank = user.rank_shorthand;
-        if(user.status === "active")
-        {
-            if (Enlisted.includes(shortRank)) {
-                if (!mem.Roles.includes('Enlisted')) {
-                    mem.addRole('Enlisted');
-                    mem.addRole('Active');
-                }
-            } else if (NCO.includes(shortRank)) {
-                if (!mem.Roles.includes('NCO')) {
-                    mem.addRole('NCO');
-                    mem.addRole('Active');
-                }
-            } else if (Officer.includes(shortRank)) {
-                if (!mem.Roles.includes('Officer')) {
-                    mem.addRole('Officer');
-                    mem.addRole('Active');
-                }
-            }
-        } else if(user.status === "disch")
-        {
-            if(user.primary_position === "Retired")
+                // Sync Ranks
+            let shortRank = user.rank_shorthand;
+            if(user.status === "active")
             {
-                mem.removeRoles(mem.roles);
-                mem.addRole('Retired');
-            }else if(user.primary_position === "Discharged")
+                if (Enlisted.includes(shortRank)) {
+                    if (!user.Roles.includes('Enlisted')) {
+                        user.addRole('Enlisted');
+                        user.addRole('Active');
+                    }
+                } else if (NCO.includes(shortRank)) {
+                    if (!user.Roles.includes('NCO')) {
+                        user.addRole('NCO');
+                        user.addRole('Active');
+                    }
+                } else if (Officer.includes(shortRank)) {
+                    if (!user.Roles.includes('Officer')) {
+                        user.addRole('Officer');
+                        user.addRole('Active');
+                    }
+                }
+            } else if(user.status === "disch")
             {
-                mem.removeRoles(discordProfile.roles);
-                mem.addRole('Discharged');
+                if(user.primary_position === "Retired")
+                {
+                    user.removeRoles(mem.roles);
+                    user.addRole('Retired');
+                }else if(user.primary_position === "Discharged")
+                {
+                    user.removeRoles(discordProfile.roles);
+                    user.addRole('Discharged');
+                }
             }
-            else {
-                // Error out.
-            }
-        }
         })
     })
 
