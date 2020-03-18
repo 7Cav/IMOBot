cfdf544b6db990129bee5393d68dd144d8845840
VexA3 <vexhd0@gmail.com>
2020-03-18 18:38:48 -0500

    Remove each managed role via loop

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const botLogin = config.Login;
 
 // Prevent the bot from pausing due to the amount of listeners being emitted
 // when the function at line 141 runs.
-require('events').EventEmitter.prototype._maxListeners = 0;
+// require('events').EventEmitter.prototype._maxListeners = 0;
 
 // Crash reporting
 bot.on('disconnect', () => console.error('Connection Lost...'));
@@ -85,7 +85,7 @@ let users = require('./mock-db.json').data.users;
 bot.on('ready', () => {
     console.log("Connected as " + bot.user.tag);
     
-    let members = await bot.guilds.get(config.DiscordServerID).members;
+    let members = bot.guilds.get(config.DiscordServerID).members;
 
     users.forEach(user => {
         syncDiscordUser(user.discord_id, user);
@@ -120,49 +120,56 @@ bot.on("guildMemberAdd", member => {
 bot.login(botLogin).catch(err => console.log(err));
 
 
-function syncDiscordUser(discordId, cavUser = null) {
+async function syncDiscordUser(discordId, cavUser = null) {
 
     var apiUserObject = null;
     if (cavUser == null) {
         // attempt to get Cav User via api/user/discord/{id}
         // JARVIS NEEDS TO IMPLEMENT THIS ENDPOINT
-        // apiUserObject = await goGetApiUserObject(discordId);
+        // apiUserRequest = await goGetApiUserObject(discordId);
 
         // if we couldn't find a cav user for the discord id, return
-        if (apiUserObject == {}) {
+        if (apiUserRequest == {}) {
             return;
+        } else {
+            cavUser = apiUserObject.user;
         }
     }
 
-    let discordProfile = await members.get(discordId);
+    let discordServer = bot.guilds.get(config.DiscordServerID);
+    let discordProfile = discordServer.members.get(discordId);
 
-    discordProfile.removeRole(discordProfile.roles);
+    await Object.values(config.MANAGED_GROUPS).forEach(groupID => {
+        discordServer.roles.has(groupID)
+        ? discordProfile.removeRole(discordServer.roles.get(groupID))
+        : console.log('fuck');
+    });
 
-    let rankShortName = user.rank_shorthand;
+    let rankShortName = cavUser.rank_shorthand;
 
-    if (user.status == 'disch') {
-        if (user.primary_position == Roster.RETIRED) {
-            discordProfile.addRole(config.GROUP_RETIRED_ID);
+    if (cavUser.status == 'disch') {
+        if (cavUser.primary_position == Roster.RETIRED) {
+            discordProfile.addRole(config.MANAGED_GROUPS.GROUP_RETIRED_ID);
             return;
         }
 
         // if the user is discharged, but not retired, we don't care
         // what their billet is. Just give them discarged and move
         // on to the next user
-        discordProfile.addRole(config.GROUP_DISCHARGED_ID);
+        discordProfile.addRole(config.MANAGED_GROUPS.GROUP_DISCHARGED_ID);
         return;
     }
 
     // all non discharged members need the active role
-    discordProfile.addRole(config.GROUP_ACTIVE_ID);
+    discordProfile.addRole(config.MANAGED_GROUPS.GROUP_ACTIVE_ID);
 
     if (ranks.NCO.includes(rankShortName)) {
-        discordProfile.addRole(config.GROUP_NCO_ID);
+        discordProfile.addRole(config.MANAGED_GROUPS.GROUP_NCO_ID);
         return;
     }
 
     if (ranks.OFFICER.includes(rankShortName)) {
-        discordProfile.addRole(config.GROUP_OFFICER_ID);
+        discordProfile.addRole(config.MANAGED_GROUPS.GROUP_OFFICER_ID);
         return;
     }    
 }
