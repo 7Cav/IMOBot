e9ebd2d0b11d05c3dbd1600e088589ccf1076d6a
VexA3 <vexhd0@gmail.com>
2020-04-22 23:21:45 -0500

    Add reaction based role - config based

--- index.js
+++ index.js
@@ -401,3 +401,83 @@ async function syncDiscordUser(discordId, cavUser = null) {
     await discordProfile.roles.add(rolesToSet)
         .catch(console.error);
 }
+
+/*
+    REACT TO ROLES || START
+*/
+
+bot.on('raw', event => {
+    const eventName = event.t;
+
+    if(eventName === 'MESSAGE_REACTION_ADD') {
+        if(event.d.message_id === cfg.React.message_id) {
+            var reactionChannel = bot.channels.get(event.d.channel_id);
+            if(reactionChannel.messages.has(event.d.message_id)) {
+                return;
+            } else {
+                reactionChannel.fetchMessage(event.d.message_id)
+                .then(msg => {
+                    var msgReaction = msg.reactions.get(event.d.emoji.name + ":" + event.d.emoji.id);
+                    var user = bot.users.get(event.d.user_id);
+                    bot.emit('messageReactionAdd', msgReaction, user);
+                })
+                .catch(err => console.log(err));
+            }
+        }
+    }else if(eventName === 'MESSAGE_REACTION_REMOVE')
+    {
+        if(event.d.message_id === cfg.React.message_id)
+        {
+            var reactionChannel = bot.channels.get(event.d.channel_id)
+            if(reactionChannel.messages.has(event.d.message_id))
+                return;
+            else {
+                reactionChannel.fetchMessage(event.d.message_id)
+                .then(msg => {
+                    var msgReaction = msg.reactions.get(event.d.emoji.name + ":" + event.d.emoji.id);
+                    var user = bot.users.get(event.d.user_id);
+                    bot.emit('messageReactionRemove', msgReaction, user);
+                })
+                .catch(err => console.log(err));
+            }
+        }
+    }
+})
+
+// part 2 for the above 'MESSAGE_REACTION_ADD'
+bot.on('messageReactionAdd', (messageReaction, user) => {
+    var roleName = messageReaction.emoji.name;
+    var role = messageReaction.message.guild.roles.find(role => role.name.toLowerCase() === 
+    roleName.toLowerCase());
+
+    if(role) {
+        var member = messageReaction.message.guild.members.find(member => member.id === user.id);
+        if(member)
+        {
+            member.addRole(role.id);
+            console.log("Success! Added role to user") 
+            member.send("You've been added to " + role.name)
+        }
+    }
+})
+
+// part 2 for the above 'MESSAGE_REACTION_REMOVE'
+bot.on('messageReactionRemove', (messageReaction, user) => {
+    var roleName = messageReaction.emoji.name;
+    var role = messageReaction.message.guild.roles.find(role => role.name.toLowerCase() === 
+    roleName.toLowerCase());
+
+    if(role) {
+        var member = messageReaction.message.guild.members.find(member => member.id === user.id);
+        if(member)
+        {
+            member.removeRole(role.id);
+            console.log("Success! Removed role from user") 
+            member.send("You've been Remvoed from " + role.name)
+        }
+    }
+})
+
+/*
+    REACT TO ROLES || END
+*/
